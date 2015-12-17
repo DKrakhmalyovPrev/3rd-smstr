be533f1407c50a875c45392ee1111f04e1375862
fredy4eg <fredy97@mail.ru>
2015-12-17 03:24:06 +0300

    Update ukkonen.cpp

--- ukkonen.cpp
+++ ukkonen.cpp
@@ -14,40 +14,75 @@
 #define INF 1000000000 
 using namespace std;
 
-//Тот же самый код, прокомментированный:
-
+// дописать к дереву символ c
 void ukkadd(int c, string in_str, int t[][26], int *l_edge, int *r_edge, int * par, int* s_link,
-	int cur_t, int cur_pos, int num_t, int cur_symb) { // дописать к дереву символ c
-suff:;      // будем приходить сюда после каждого перехода к суффиксу (и заново добавлять символ)
-	if (r_edge[cur_t]<cur_pos) { // проверим, не вылезли ли мы за пределы текущего ребра
-		// если вылезли, найдем следующее ребро. Если его нет - создадим лист и прицепим к дереву
-		if (t[cur_t][c] == -1) {
-			t[cur_t][c] = num_t; l_edge[num_t] = cur_symb; par[num_t++] = cur_t; cur_t = s_link[cur_t];
-		cur_pos = r_edge[cur_t] + 1; goto suff; }
-		cur_t = t[cur_t][c]; cur_pos = l_edge[cur_t]; // в противном случае просто перейдем к следующему ребру
-	}
-	if (cur_pos == -1 || c == in_str[cur_pos] - 'a') cur_pos++; else { // если буква на ребре совпадает с c то идем по ребру, а иначе
+	int cur_t, int cur_pos, int num_t, int cur_symb) 
+{ 
+// будем приходить сюда после каждого перехода к суффиксу (и заново добавлять символ)
+
+suff:;   
+// проверим, не вылезли ли мы за пределы текущего ребра
+// если вылезли, найдем следующее ребро. Если его нет - создадим лист и прицепим к дереву
+// в противном случае просто перейдем к следующему ребру
+	if (r_edge[cur_t] < cur_pos)
+	{
+		if (t[cur_t][c] == -1)
+		{
+			t[cur_t][c] = num_t;
+			l_edge[num_t] = cur_symb;
+			par[num_t++] = cur_t;
+			cur_t = s_link[cur_t];
+			cur_pos = r_edge[cur_t] + 1;
+			goto suff;
+		}
+		else
+		{
+			cur_t = t[cur_t][c];
+			cur_pos = l_edge[cur_t]; // в противном случае просто перейдем к следующему ребру
+		}
+		// если буква на ребре совпадает с c то идем по ребру, а иначе
 		// разделяем ребро на два. Посередине - вершина num_t
-		l_edge[num_t] = l_edge[cur_t]; r_edge[num_t] = cur_pos - 1; par[num_t] = par[cur_t]; t[num_t][in_str[cur_pos] - 'a'] = cur_t;
-		// ставим лист num_t+1. Он соответствует переходу по c.
-		t[num_t][c] = num_t + 1; l_edge[num_t + 1] = cur_symb; p[num_t + 1] = num_t;
-		// обновляем параметры текущей вершины. Не забыть про переход от предка cur_t к num_t.
-		l_edge[cur_t] = cur_pos; par[cur_t] = num_t; t[par[num_t]][in_str[l_edge[num_t]] - 'a'] = num_t; num_t += 2;
-		// готовимся к спуску: поднялись на ребро и перешли по суффиксной ссылке.
-		// cur_pos будет отмечать, где мы в текущем суффиксе.
-		cur_t = s[par[num_t - 2]]; cur_pos = l_edge[num_t - 2];
-		// пока текущий суффикс не кончился, топаем вниз
-		while (cur_pos <= r_edge[num_t - 2]) { cur_t = t[cur_t][in_str[cur_pos] - 'a']; cur_pos += r_edge[cur_t] - l_edge[cur_t] + 1; }
-		// если мы пришли в вершину, то поставим в нее суффиксную ссылку, иначе поставим в num_t
-		// (ведь на след. итерации мы создадим num_t).
-		if (cur_pos == r_edge[num_t - 2] + 1) s_link[num_t - 2] = cur_t; else s_link[num_t - 2] = num_t;
-		// устанавливаем cur_pos на новое ребро и идем добавлять букву к суффиксу.
-		cur_pos = r_edge[cur_t] - (cur_pos - r_edge[num_t - 2]) + 2; goto suff;
+		if (cur_pos == -1 || c == in_str[cur_pos] - 'a')
+			cur_pos++;
+		else
+		{
+			l_edge[num_t] = l_edge[cur_t];
+			r_edge[num_t] = cur_pos - 1;
+			par[num_t] = par[cur_t];
+			t[num_t][in_str[cur_pos] - 'a'] = cur_t;
+			// ставим лист num_t+1. Он соответствует переходу по c.
+			t[num_t][c] = num_t + 1;
+			l_edge[num_t + 1] = cur_symb;
+			par[num_t + 1] = num_t;
+			// обновляем параметры текущей вершины. Не забыть про переход от предка cur_t к num_t.
+			l_edge[cur_t] = cur_pos;
+			par[cur_t] = num_t;
+			t[par[num_t]][in_str[l_edge[num_t]] - 'a'] = num_t;
+			num_t += 2;
+			// готовимся к спуску: поднялись на ребро и перешли по суффиксной ссылке.
+			// cur_pos будет отмечать, где мы в текущем суффиксе.
+			cur_t = s_link[par[num_t - 2]];
+			cur_pos = l_edge[num_t - 2];
+			// пока текущий суффикс не кончился, топаем вниз
+			while (cur_pos <= r_edge[num_t - 2])
+			{
+				cur_t = t[cur_t][in_str[cur_pos] - 'a'];
+				cur_pos += r_edge[cur_t] - l_edge[cur_t] + 1;
+			}
+			// если мы пришли в вершину, то поставим в нее суффиксную ссылку, иначе поставим в num_t
+			// (ведь на след. итерации мы создадим num_t).
+			if (cur_pos == r_edge[num_t - 2] + 1)
+				s_link[num_t - 2] = cur_t;
+			else
+				s_link[num_t - 2] = num_t;
+			// устанавливаем cur_pos на новое ребро и идем добавлять букву к суффиксу.
+			cur_pos = r_edge[cur_t] - (cur_pos - r_edge[num_t - 2]) + 2;
+			goto suff;
+		}
 	}
 }
 
-void build(string in_str, int t[][26], int *l_edge, int *r_edge, int * par, int* s_link,
-	int cur_t, int cur_pos,int num_t,int cur_symb) 
+void build(string in_str, int t[][26], int *l_edge, int *r_edge, int * par, int* s_link,int cur_t, int cur_pos,int num_t,int cur_symb) 
 {
 	num_t = 2;
 	cur_t = 0;
