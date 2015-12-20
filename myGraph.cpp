c4b9bb485f0b8cadcf46544ae7272c274d177bee
fredy4eg <fredy97@mail.ru>
2015-12-21 01:45:38 +0300

    Update myGraph.cpp

--- myGraph.cpp
+++ myGraph.cpp
@@ -31,13 +31,56 @@ public:
 		matr = x.matr;
 	}
 
+	Graph(const Graph& x, const Graph& y)
+	{
+		map<top, data> l;
+		for (int i = 0; i < x.Size()+y.Size(); i++)
+			matr.push_back(l);
+		for (auto it = x.matr[0].start(); it != x.matr[0].end(); it++)
+			matr[0][it.first] = it.second;
+		for (auto it = y.matr[0].start(); it != y.matr[0].end(); it++)
+			matr[0][it.first + x.size() - 1] = it.second;
+		for (int i = 1; i < x.Size(); i++)
+		{
+			for (auto it = x.matr[i].start(); it != x.matr[i].end(); it++)
+				matr[i][it.first] = it.second*0.8;
+			for (auto it = y.matr[i].start(); it != y.matr[i].end(); it++)
+				matr[i][it.first + x.size() - 1] = it.second;
+		}
+		for (int i = x.Size(); i < y.Size()+x.Size(); i++)
+		{
+			for (auto it = x.matr[i].start(); it != x.matr[i].end(); it++)
+				matr[i][it.first] = it.second;
+			for (auto it = y.matr[i].start(); it != y.matr[i].end(); it++)
+				matr[i][it.first - x.size()] = it.second*0.8;
+		}
+
+	}
+
+	void ScanGraph()
+	{
+		int tops;
+		cin >> tops;
+		for (int i = 0; i < tops; i++)
+		{
+			int rib;
+			cin >> rib;
+			for (int j = 0; j < tops; j++)
+			{
+				int l, m;
+				cin >> l >> m;
+				matr[i][l] = m;
+			}
+		}
+	}
+
 	void SetRib(int a, top b, data s)
 	{
 		matr[a][b] = s;
 	}
 
 
-	int GetRib(int a, top b)
+	int GetRib(int a, top b) const
 	{
 		return(matr[a][b]);
 	}
@@ -53,63 +96,157 @@ public:
 		return(matr.size());
 	}
 
-	void SetFin(int i)
+	vector<int> BFS(int start) const
 	{
-		ifFin[i] = 1;
+		vector<int> result(matr.size(), -1);
+		vector<bool> ifbeen(matr.size(), 0);
+		queue<int> que;
+		que.push(start);
+		ifbeen[start] = 1;
+		result[start] = 0;
+		while (!que.empty())
+		{
+			int ti = que.front();
+			que.pop();
+			for (auto it = matr[ti].start(); it<matr[ti].end();it++)
+			if ((ifbeen[it.first()] == 0) || (result[it.first()]>result[ti] + 1))
+			{
+				ifbeen[it.first()] = 1;
+				result[it.first()] = result[ti] + 1;
+				que.push(it.first());
+			}
+		}
+		return(result);
 	}
 
+	int BFS(int start, int finish) const
+	{
+		return(BFS(start)[finish]);
+	}
+
+	void step(int cur, vector<int> & res, vector<bool> & ifbeen)
+	{
+		ifbeen[cur] = 1;
+		for (auto it = matr[ti].start(); it<matr[ti].end(); it++)
+		if ((ifbeen[it.first()] == 0) || (res[it.first()]>res[cur] + 1))
+		{
+			res[it.first()] = res[cur] + 1;
+			step(it.first(), res, ifbeen);
+		}
+	}
+
+	vector<int> DFS(int start)
+	{
+		vector<int> result(matr.size(), -1);
+		vector<bool> ifbeen(matr.size(), 0);
+		stack<int> st;
+		st.push(start);
+		ifbeen[start] = 1;
+		result[start] = 0;
+		step(start, result, ifbeen);
+		return(result);
+	}
+
+	int DFS(int start, int finish)
+	{
+		return(DFS(start)[finish]);
+	}
+		
+
+	vector<int> Dijkstra(int start) const
+	{
+		vector<int> result(matr.size(), INT_MAX);
+		vector<bool> ifbeen(matr.size(), 0);
+		queue<int> que;
+		que.push(start);
+		ifbeen[start] = 1;
+		result[start] = 0;
+		while (!que.empty())
+		{
+			int ti = que.front();
+			que.pop();
+			for (auto it = matr[ti].start(); it<matr[ti].end(); it++)
+			if ((ifbeen[it.first()] == 0) || (result[it.first()]>result[ti] + it.second()))
+			{
+				result[it.first()] = result[ti] + it.second();
+				ifbeen[it.first()] = 1;
+				que.push(it.first());
+			}
+		}
+		return(result);
+	}
+
+	int Dijkstra(int start, int finish) const
+	{
+		return(Dijkstra(start)[finish]);
+	}
 	int Find(int i, top s)
-	{
-		if (matr[i].find(s) != matr[i].end())
-			return(matr[i][s]);
-		else
-			return(0);
-	}
+		{
+			if (matr[i].find(s) != matr[i].end())
+				return(matr[i][s]);
+			else
+				return(0);
+		}
 
+		
 	void recLinks(vector<int>& links, int fathT, top symb)
-	{
-		if (matr[links[fathT]].find(symb) != matr[links[fathT]].end())
-		if (matr[links[fathT]][symb] != matr[fathT][symb])
 		{
-			links[matr[fathT][symb]] = matr[links[fathT]][symb];
+			if (matr[links[fathT]].find(symb) != matr[links[fathT]].end())
+			if (matr[links[fathT]][symb] != matr[fathT][symb])
+			{
+				links[matr[fathT][symb]] = matr[links[fathT]][symb];
+			}
+			else
+				links[matr[fathT][symb]] = 0;
+			for (auto it = matr[matr[fathT][symb]].begin(); it != matr[matr[fathT][symb]].end(); it++)
+			{
+				recLinks(links, matr[fathT][symb], it->first);
+			}
 		}
-		else
-			links[matr[fathT][symb]] = 0;
-		for (auto it = matr[matr[fathT][symb]].begin(); it != matr[matr[fathT][symb]].end(); it++)
-		{
-			recLinks(links, matr[fathT][symb], it->first);
-		}
-	}
 
+		
 	vector<int> Suff_link()
-	{
-		vector<int> links;
-		for (int i = 0; i < matr.size(); i++)
-			links.push_back(0);
-		for (auto it = matr[0].begin(); it != matr[0].end(); it++)
 		{
-			recLinks(links, 0, it->first);
+			vector<int> links;
+			for (int i = 0; i < matr.size(); i++)
+				links.push_back(0);
+			for (auto it = matr[0].begin(); it != matr[0].end(); it++)
+			{
+				recLinks(links, 0, it->first);
+			}
+			return(links);
 		}
-		return(links);
+
+		
+	vector<int> Fin_link(vector<int> links, vector<int> ifFin)
+		{
+			vector<int> finlinks;
+			for (int i = 0; i < matr.size(); i++)
+				finlinks.push_back(0);
+			for (int i = 0; i < links.size(); i++)
+			if (ifFin[i] == 1)
+			if (ifFin[links[i]] == 1)
+				finlinks[i] = links[i];
+			return(finlinks);
+		}
+
+	vector<double> transport(Graph& second, int start) const
+	{
+		Graph i(this, second);
+		return(i.Dijkstra(start));
 	}
 
-	vector<int> Fin_link(vector<int> links, vector<int> ifFin)
+	double transport(Graph& second, int start, int finish) const
 	{
-		vector<int> finlinks;
-		for (int i = 0; i < matr.size(); i++)
-			finlinks.push_back(0);
-		for (int i = 0; i < links.size(); i++)
-		if (ifFin[i] == 1) 
-		if (ifFin[links[i]] == 1)
-			finlinks[i] = links[i];
-		return(finlinks);
+		return(transport(second, start)[finish]);
 	}
+
 };
 
 Graph<char,int> ParsePat(vector<string>& pat, vector<int>& ifFin)
 {
 	Graph<char,int> bor(1);
-	for (int i = 0; i < pat.size(), i++)
+	for (int i = 0; i < pat.size(); i++)
 		ifFin.push_back(0);
 	for (int i = 0; i < pat.size(); i++)
 	{
